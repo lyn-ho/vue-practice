ee42282277045ed8850812c0e354fc3dd1e62f9b
Lyn Ho <lyn1107130806@gmail.com>
2019-03-20 18:17:39 +0800

    Chapter 1.2

---
+++ src/views/1.2/PersonalInfo.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  div
+    select(:value='phoneInfo.areaCode'
+    placeholder='area code'
+    @change='handleAreaCodeChange')
+      option(value='+86') +86
+      option(value='+60') + 60
+    
+    input(:value='phoneInfo.phone'
+    type='number'
+    placeholder='phone number'
+    @input='handlePhoneChange')
+
+    input(:value='zipCode'
+    type='number'
+    placeholder='zipcode'
+    @input='handleZipCodeChange')
+</template>
+
+<script>
+export default {
+  name: 'PersonalInfo',
+
+  model: {
+    prop: 'phoneInfo',
+    event: 'change'
+  },
+
+  props: {
+    phoneInfo: Object,
+    zipCode: String
+  },
+
+  methods: {
+    handleAreaCodeChange(e) {
+      this.$emit('change', {
+        ...this.phoneInfo,
+        areaCode: e.target.value
+      })
+    },
+
+    handlePhoneChange(e) {
+      this.$emit('change', {
+        ...this.phoneInfo,
+        phone: e.target.value
+      })
+    },
+
+    handleZipCodeChange(e) {
+      this.$emit('update:zipCode', e.target.value)
+    }
+  },
+}
+</script>
+
+
--- src/views/1.2/index.vue
+++ src/views/1.2/index.vue
@@ -1,10 +1,33 @@
 <template lang="pug">
-  
+  div
+    PersonalInfo(:phone-info='phoneInfo'
+    :zip-code='zipCode'
+    @change='val => (phoneInfo = val)'
+    @update:zipCode='val => (zipCode = val)')
+
+    | phoneInfo: {{ phoneInfo }}
+    br
+    | zipCode: {{ zipCode }}
 </template>
 
 <script>
+import PersonalInfo from './PersonalInfo'
+
 export default {
-  
+  components: {
+    PersonalInfo
+  },
+
+  data() {
+    return {
+      phoneInfo: {
+        areaCode: '+86',
+        phone: ''
+      },
+
+      zipCode: ''
+    }
+  }
 }
 </script>
 
