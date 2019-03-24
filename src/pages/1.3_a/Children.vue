973f03e37fe0d31c157a8bbbda337736db15ffee
Lyn Ho <lyn1107130806@gmail.com>
2019-03-24 16:33:04 +0800

    Chapter 1.3 answer.

---
+++ src/pages/1.3_a/Children.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div.border2
+    input(v-model='phone' type='number')
+    slot
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      phone: ''
+    }
+  }
+}
+</script>
+
--- src/pages/1.3_a/index.vue
+++ src/pages/1.3_a/index.vue
@@ -1,10 +1,39 @@
 <template lang="pug">
-  
+  div
+    a-tabs
+      a-tab-pane(key='ChildrenA1' tab='index key')
+        ChildrenA1
+      a-tab-pane(key='ChildrenA2' tab='static key')
+        ChildrenA2
 </template>
 
 <script>
+import ChildrenA1 from './ChildrenA1'
+import ChildrenA2 from './ChildrenA2'
+
 export default {
-  
+  components: {
+    ChildrenA1,
+    ChildrenA2
+  }
 }
 </script>
+
+<style>
+.border,
+.border1,
+.border2 {
+  border: 1px solid black;
+  padding: 10px 0;
+  margin: 10px 10px 0;
+}
+
+.border1 {
+  border-color: #ccc;
+}
+
+.border2 {
+  border-color: #eee;
+}
+</style>
 
