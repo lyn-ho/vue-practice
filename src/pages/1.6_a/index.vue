f7c340a36ecc4b8f2cab5a027ffd9b2a87fc503c
Lyn Ho <lyn1107130806@gmail.com>
2019-03-25 10:06:30 +0800

    Chapter 1.6 answer.

--- src/pages/1.6_a/index.vue
+++ src/pages/1.6_a/index.vue
@@ -1,10 +1,23 @@
 <template lang="pug">
-  
+  div
+    Spike(:start-time='startTime' :end-time='endTime')
 </template>
 
 <script>
+import moment from 'moment'
+import Spike from './Spike'
+
 export default {
-  
+  components: {
+    Spike
+  },
+
+  data() {
+    return {
+      startTime: moment('2019-03-28 14:05:00'),
+      endTime: moment('2019-03-28 14:20:00')
+    }
+  }
 }
 </script>
 
